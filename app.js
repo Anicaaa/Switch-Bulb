d0d250a0a143e1501e97ea99bf100385953e8e29
Anica Rahman <anicarahman2009@yahoo.it>
2022-09-01 12:10:36 +0100

    h1 not positioned correctly

---
+++ app.js
@@ -0,0 +1,31 @@
+// Select elements
+const btnOff = document.querySelector(".btn-off");
+const btnOn = document.querySelector(".btn-on");
+const container = document.querySelector(".light-container");
+
+// Swith ON + Add sentence once light is switched on
+function bulbOn() {
+  const textAdd = document.createElement("h1");
+  textAdd.innerText = "I have an idea!";
+  container.append(textAdd);
+
+  const lightOn = document.querySelector(".bulb-off");
+  lightOn.setAttribute("src", "BulbOn.png");
+}
+
+btnOn.addEventListener("click", function () {
+  bulbOn();
+});
+
+// Switch OFF
+function bulbOff() {
+  const textRemove = document.querySelector("h1");
+  textRemove.remove();
+
+  const lightOff = document.querySelector(".bulb-off");
+  lightOff.setAttribute("src", "BulbOff.png");
+}
+
+btnOff.addEventListener("click", function () {
+  bulbOff();
+});
