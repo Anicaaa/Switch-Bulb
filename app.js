2d4d0c41614c4d9f8834b426149df52f9290432f
Anica Rahman <anicarahman2009@yahoo.it>
2022-09-01 14:39:38 +0100

    Complete

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // Select elements
 const btnOff = document.querySelector(".btn-off");
 const btnOn = document.querySelector(".btn-on");
-const container = document.querySelector(".light-container");
+const container = document.querySelector(".img-container");
 
 // Swith ON + Add sentence once light is switched on
 function bulbOn() {
   const textAdd = document.createElement("h1");
   textAdd.innerText = "I have an idea!";
-  container.append(textAdd);
+  textAdd.setAttribute("class", "centered");
+  container.prepend(textAdd);
 
   const lightOn = document.querySelector(".bulb-off");
   lightOn.setAttribute("src", "BulbOn.png");
