ec07c7326e13ab60a1547002a8b9923d55fe7aa3
Anica Rahman <anicarahman2009@yahoo.it>
2022-09-30 18:43:36 +0100

    Changed light bulb

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 // Select elements
 const btnOff = document.querySelector(".btn-off");
 const btnOn = document.querySelector(".btn-on");
-const container = document.querySelector(".img-container");
 
 // Swith ON + Add sentence once light is switched on
 function bulbOn() {
-  const textAdd = document.createElement("h1");
-  textAdd.innerText = "I have an idea!";
-  textAdd.setAttribute("class", "centered");
-  container.prepend(textAdd);
-
   const lightOn = document.querySelector(".bulb-off");
   lightOn.setAttribute("src", "BulbOn.png");
 }
@@ -20,9 +14,6 @@ btnOn.addEventListener("click", function () {
 
 // Switch OFF
 function bulbOff() {
-  const textRemove = document.querySelector("h1");
-  textRemove.remove();
-
   const lightOff = document.querySelector(".bulb-off");
   lightOff.setAttribute("src", "BulbOff.png");
 }
